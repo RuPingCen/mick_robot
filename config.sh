5c129591b4b66be925741657b106c1177eb2cbcf
rupingcen <cenruping@vip.qq.com>
2023-05-09 12:18:29 +0800

    simplify dependent lib

--- config.sh
+++ config.sh
@@ -1,7 +1,3 @@
-sudo apt-get install ros-kinetic-serial ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-gmapping ros-kinetic-move-base
-    
-sudo apt-get install ros-kinetic-tf*
-
+sudo apt-get install ros-melodic-serial ros-melodic-amcl ros-melodic-map-server ros-melodic-gmapping ros-melodic-move-base
+sudo apt-get install ros-melodic-tf* ros-melodic-teleop-twist-keyboard
 sudo apt-get install libpcap-dev
-
-sudo apt-get install ros-kinetic-teleop-twist-keyboard
